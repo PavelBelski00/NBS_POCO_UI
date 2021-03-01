1f6ca7c1a0f388880152ebd345a10ca71a0c418b
KarinaBlagushko <blagushkokarian@gmail.com>
2021-03-01 13:59:38 +0000

    order ovp

---
+++ report.ovporder/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ovp/app/Component'], function(AppComponent) {
+    return AppComponent.extend("nbs.poc.ovp.ui.app.report.ovporder.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
